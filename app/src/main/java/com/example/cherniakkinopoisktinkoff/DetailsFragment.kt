9859a3089d012ed48cb0c430ff8c7e0659a4296c
Anton Chernyak <eldariliona@gmail.com>
2023-02-04 03:55:05 +0300

    added DetailsFragment layout

--- app/src/main/java/com/example/cherniakkinopoisktinkoff/DetailsFragment.kt
+++ app/src/main/java/com/example/cherniakkinopoisktinkoff/DetailsFragment.kt
@@ -6,14 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-
-class ListFragment : Fragment() {
+class DetailsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        return inflater.inflate(R.layout.fragment_details, container, false)
     }
 
 }
